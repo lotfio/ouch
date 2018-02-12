a9eae0e2b3a8e26f18a5ef483925f89770eed188
lotfio-lakehal <lotfiolakehal@gmail.com>
2018-02-12 17:34:20 +0100

    adding exception classes

---
+++ src/Ouch/Exceptions/DepricatedException.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Ouch error handler for PHP
+ *
+ * @package     Ouch
+ * @author      Lotfio Lakehal <lotfiolakehal@gmail.com>
+ * @copyright   2018 Lotfio Lakehal
+ * @license     MIT
+ * @link        https://github.com/lotfio-lakehal/ouch
+ */
+
+namespace Ouch\Exceptions;
+
+class DepricatedException extends ErrorException
+{
+    
+}
