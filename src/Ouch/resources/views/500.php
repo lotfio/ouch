994079c8048202e8204310a187d40d86ce46d815
lotfio-lakehal <lotfiolakehal@gmail.com>
2018-02-12 23:25:59 +0100

    update view

--- src/Ouch/resources/views/500.php
+++ src/Ouch/resources/views/500.php
@@ -8,45 +8,53 @@
     <!-- spectre -->
     <style>
         <?php include ouch_assets('css/spectre.min.css')?>
+        <?php include ouch_assets('css/bootstrap.min.css')?>
         <?php include ouch_assets('css/prism.min.css')?>
         <?php include ouch_assets('css/custom.css')?>
     </style>
 
-    <title>Ouch | <?=$ex->class?></title>
+    <title>Ouch | <?=str_last($ex->class)?></title>
 </head>
 <body>
 
 <header>
-    <div class="container grid-lg text-center">
-        <!--<h2>Nav here</h2>-->
-    </div>
 
-    <section class="text-center error-section">
+    <section class="error-section text-center">
         <div class="container">
-            <h1><?=$ex->class?></h1>
-            <h5><?= $ex->message . ' in line: <span class="error-line">'.$ex->line . '</span>' ?></h5>
+        
+            <div class="row">
+                <div class="col-xl-3 col-lg-3 col-sm-12">
+                    <img src="https://www.sans.org/sites/default/files/ouch.png" alt="logo" class="text-center">
+                </div>
+
+                <div class="col-xl-9 col-lg-9 col-sm-12">
+                    <h1><?=str_last($ex->class)?></h1>
+                    <h5><?= $ex->message . ' in line: <span class="error-line">'.$ex->line . '</span>' ?></h5>
+                </div>
+            </div>
         </div>
     </section>
 
 </header>
 
 <section class="">
-    <div class="container grid-xl">
-        <div class="columns">
+    <div class="container">
+        <div class="row">
 
-            <div class="column">
+            <div class="col-md-12">
 
                 <div class="text-editor">
                     <h5>Line <?=$ex->line?> in file <?=$ex->file?> : </h5>
                     <pre class="language-php"><code><?=readErrorFile($ex->file, $ex->line)?></code></pre>
                     
-                    <?php  if(isset($ex->trace[0])) print_r($ex->trace[0])  ?>
+                    <h4 class="">Icons : </h4>
                 </div>
 
             </div>
 
-            <div class="column">
+            <div class="col-md-4">
                 <div class="server-info">
+                <?php  if(isset($ex->trace[0])) print_r($ex->trace[0])  ?>
                   <h5>Server info: </h5>
                   <?php
                     foreach($_SERVER as $key => $val)
