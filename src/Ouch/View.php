bb5d346fc234795f1e8147a551942b0a3f12f32c
lotfio <contact@lotfio.net>
2019-08-21 22:20:24 +0100

    adding cli view

--- src/Ouch/View.php
+++ src/Ouch/View.php
@@ -14,6 +14,10 @@ namespace Ouch;
 
 class View
 {
+    public static function writeLn($line, $color = 37, $bg = 48, $bold = 0)
+    {
+        return fwrite(STDOUT,"\e[".$bold.';'.$color.';'.$bg.'m'.$line."\e[0m");
+    }
     /**
      * render view method.
      *
@@ -26,8 +30,35 @@ class View
     {
         // TODO create a nice cli view
         if (php_sapi_name() === 'cli') {
-            exit(json_encode($ex));
-        }
+try{
+
+
+            self::writeLn("\n   ");
+            self::writeLn($ex->class, "37", "41");
+            self::writeLn("\n");
+            self::writeLn("\n   => Type    :  ", "33");
+
+
+            
+            self::writeLn(($ex->type), "32");
+            self::writeLn("\n   => Message :  ", "33");
+            self::writeLn(($ex->message), "32");
+
+            self::writeLn("\n   => File    :  ", "33");
+            self::writeLn(($ex->file), "32");
+
+            self::writeLn("\n   => Line    :  ", "33");
+            self::writeLn(($ex->line), "32");
+
+            self::writeLn("\n   => Trace   :  ", "33");
+            self::writeLn(json_encode($ex->trace), "32");
+            self::writeLn("\n");
+}catch(\Exception $e)
+{
+    die($e->getMessage());
+}
+            die;
+        }   
 
         http_response_code(500);
         $view = require ouch_views($file);
